7e841a6185afec06ec1045a55ad47b15d830241e
Jai Lalawat <jailalawat@gmail.com>
2018-03-22 14:24:22 +0530

    ad file size validation

--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -6,12 +6,18 @@
         var file, types;
         types = /(\.|\/)(gif|jpe?g|png)$/i;
         file = data.files[0];
-        if (types.test(file.type) || types.test(file.name)) {
-          data.context = $(tmpl("template-upload", file));
-          $('#new_photo').append(data.context);
-          return data.submit();
-        } else {
-          return alert(file.name + " is not a gif, jpeg, or png image file");
+        
+        var file_size = file.size;
+        if(file_size>51000000) {
+          return alert("File size is greater than 50MB");
+        }else {
+          if (types.test(file.type) || types.test(file.name)) {
+            data.context = $(tmpl("template-upload", file));
+            $('#new_photo').append(data.context);
+            return data.submit();
+          } else {
+            return alert(file.name + " is not a gif, jpeg, or png image file");
+          }
         }
       },
       progress: function(e, data) {
